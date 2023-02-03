01828eb102f1efc7c1789939c8075b129e11cd4b
Webfael <rafaelfaustrodrigues@gmail.com>
2023-02-03 20:24:52 -0300

    menu-responsivo

---
+++ src/js/script.js
@@ -0,0 +1,10 @@
+function find_max(nums) {
+    2 const max_num = Number.NEGATIVE_INFINITY;
+    3 for (let num of nums) {
+    4 if (num > max_num) {
+    6 }
+    7 }
+    8 return max_num;
+    9 }
+
+    console.log(max_num)
